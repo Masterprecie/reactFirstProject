b4be95b2467344779108f3c5ad96f37b7906e2e1
Precious <ikpaprecious2@gmail.com>
2022-07-09 10:18:24 +0100

    responsiveness

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import logo from '../Assets/media/logo.png';
-import { IconButton, Image, Text } from '@chakra-ui/react';
-import { HStack, Heading, Spacer, Box, Link } from '@chakra-ui/react'
+import {  Image } from '@chakra-ui/react';
+import {  Heading,  Box,  Link } from '@chakra-ui/react'
 import ToggleModeBtn from './ToggleModeBtn';
-import { GiHamburgerMenu } from 'react-icons/Gi';
+
 
 const NavBar = () => {	
 
@@ -18,13 +18,6 @@ const NavBar = () => {
           <Link color='black' fontWeight='semibold'>Features</Link>
           <Link color='black'  fontWeight='semibold' pl={8}>Teams</Link>
           <Link color='black'  fontWeight='semibold' pl={8} pr={4}>Sign In</Link>
-          <IconButton
-          aria-label='Open Menu'
-          size='lg'
-          mr='2'
-          icon={<GiHamburgerMenu/>}
-          display={{base:'flex', sm:'flex', md:'flex', lg:'none'}}
-          /> 
 			 <ToggleModeBtn/>         
         </Box>  
     </Box>       
